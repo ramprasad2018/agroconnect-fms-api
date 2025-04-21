7426e15770b4af2016903a744de22b14efcb68ea
Ram Prasad Panigrahy <panigrahyr8@gmail.com>
2025-04-21 09:46:17 +0000

    Initial commnit

---
+++ src/main/java/com/agroconnect/fms_api/FmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.agroconnect.fms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroconnect/fms_api/FmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroconnect.fms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
